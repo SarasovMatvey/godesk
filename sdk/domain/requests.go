818c9e65575f70404075a624d5ae9e06a8b0223d
sarasov matvey <sarasovmatvej@gmail.com>
2021-12-08 10:33:48 +0600

    added fields

--- sdk/domain/requests.go
+++ sdk/domain/requests.go
@@ -5,7 +5,12 @@ type Request struct {
 	FirstOperatorId   interface{} `json:"first_operator_id"`
 	FirstOperatorName string      `json:"first_operator_name"`
 	RequestTime       int         `json:"request_time"`
+	RequestStart      interface{} `json:"request_start"`
+	RequestStartD     string      `json:"request_start_d"`
+	RequestStartT     string      `json:"request_start_t"`
 	ReplyStart        interface{} `json:"reply_start"`
+	ReplyStartD       string      `json:"reply_start_d"`
+	ReplyStartT       string      `json:"reply_start_t"`
 	ClientMessenger   string      `json:"client_messenger"`
 	ClientId          int         `json:"client_id"`
 	ClientName        string      `json:"client_name"`
